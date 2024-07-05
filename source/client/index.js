4b7984b600893ee87935854fb4f3f16a4de4ba5a
shivapand <shiva.pandit@ecolibrium.io>
2024-07-05 17:12:34 +0530

    init

--- source/client/index.js
+++ source/client/index.js
@@ -4,6 +4,21 @@ import 'bootstrap';
 
 import './index.scss';
 
+const data = [
+  {
+    country: 'india',
+    cities: ['delhi', 'mumbai']
+  },
+  {
+    country: 'bangladesh',
+    cities: ['dhaka', 'chittagong']
+  },
+  {
+    country: 'srilanka',
+    cities: ['colombo', 'jaffna']
+  }
+];
+
 createRoot(document.getElementById('viewer')).render(
   <pre>
     please use the data below,
@@ -12,23 +27,6 @@ createRoot(document.getElementById('viewer')).render(
     <br />
     refer https://react.dev/reference/react-dom/components/select
     <br />
-    {JSON.stringify(
-      [
-        {
-          country: 'india',
-          cities: ['delhi', 'mumbai']
-        },
-        {
-          country: 'bangladesh',
-          cities: ['dhaka', 'chittagong']
-        },
-        {
-          country: 'srilanka',
-          cities: ['colombo', 'jaffna']
-        }
-      ],
-      undefined,
-      2
-    )}
+    {JSON.stringify(data, undefined, 2)}
   </pre>
 );
