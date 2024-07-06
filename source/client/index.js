6773210efc29d0636d4652e58dd00954434b5c4a
vijay.sardhara <vijay.sardhara@suvit.io>
2024-07-06 10:57:25 +0530

    country wise state select box - added.
    country wise state select box - added.

--- source/client/index.js
+++ source/client/index.js
@@ -1,58 +1,93 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import 'bootstrap';
+import React, { useState } from 'react';
+import './App.css'; // You can define your styles in App.css
 
-// eslint-disable-next-line
-// @ts-ignore
-import finalGif from './final.gif';
-import './index.scss';
+function App() {
+  const data = [
+    {
+      country: 'India',
+      cities: [
+        { name: 'Delhi', description: 'Capital of India' },
+        { name: 'Mumbai', description: 'Financial capital of India' }
+      ]
+    },
+    {
+      country: 'Bangladesh',
+      cities: [
+        { name: 'Dhaka', description: 'Capital of Bangladesh' },
+        { name: 'Chittagong', description: 'Financial capital of Bangladesh' }
+      ]
+    },
+    {
+      country: 'Sri Lanka',
+      cities: [
+        { name: 'Colombo', description: 'Capital of Sri Lanka' },
+        { name: 'Jaffna', description: 'Financial capital of Sri Lanka' }
+      ]
+    }
+  ];
 
-const data = [
-  {
-    country: 'India',
-    cities: [
-      { name: 'Delhi', description: 'Capital of india' },
-      { name: 'Mumbai', description: 'Financial capital of india' }
-    ]
-  },
-  {
-    country: 'Bangladesh',
-    cities: [
-      { name: 'Dhaka', description: 'Capital of bangladesh' },
-      { name: 'Chittagong', description: 'Financial capital of bangladesh' }
-    ]
-  },
-  {
-    country: 'Sri-lanka',
-    cities: [
-      { name: 'Colombo', description: 'Capital of srilanka' },
-      { name: 'Jaffna', description: 'Financial capital of srilanka' }
-    ]
-  }
-];
+  const [selectedCountry, setSelectedCountry] = useState('');
+  const [selectedCity, setSelectedCity] = useState('');
+  const [cityDescription, setCityDescription] = useState('');
 
-createRoot(document.getElementById('viewer')).render(
-  <div className='m-3'>
-    please use the data at the bottom,
-    <br />
-    to build a paired-select using only the built-in browser &lt;select&gt;
-    <br />
-    <div className='text-muted'>
-      refer https://react.dev/reference/react-dom/components/select
+  const handleCountryChange = (event) => {
+    const country = event.target.value;
+    setSelectedCountry(country);
+    setSelectedCity('');
+    setCityDescription('');
+  };
+
+  const handleCityChange = (event) => {
+    const city = event.target.value;
+    setSelectedCity(city);
+
+    // Find city description from data
+    const countryData = data.find(country => country.country === selectedCountry);
+    const cityData = countryData.cities.find(c => c.name === city);
+    if (cityData) {
+      setCityDescription(cityData.description);
+    } else {
+      setCityDescription('');
+    }
+  };
+
+  return (
+    <div className="App">
+      <h2>Select a country / city</h2>
+
+      {/* Country Select */}
+      <select value={selectedCountry} onChange={handleCountryChange}>
+        <option value="">Select a country</option>
+        {data.map((country, index) => (
+          <option key={index} value={country.country}>
+            {country.country}
+          </option>
+        ))}
+      </select>
+
+      {/* City Select (Only shown when a country is selected) */}
+      {selectedCountry && (
+        <select value={selectedCity} onChange={handleCityChange}>
+          <option value="">Select a city</option>
+          {data
+            .find(country => country.country === selectedCountry)
+            .cities.map((city, index) => (
+              <option key={index} value={city.name}>
+                {city.name}
+              </option>
+            ))}
+        </select>
+      )}
+
+      {/* City Description (Only shown when a city is selected) */}
+      {selectedCity && (
+        <div className="city-description">
+          <h3>City Description:</h3>
+          <p>{cityDescription}</p>
+        </div>
+      )}
     </div>
-    conditions:
-    <ul>
-      <li>select should have placeholders (Select a country / city)</li>
+  );
+}
 
-      <li>city-select should be displayed only when a country is selected</li>
-
-      <li>submit should be displayed only when a city is selected</li>
-
-      <li>on submit the city description should be displayed</li>
-
-      <li>the components should be centered to the page</li>
-    </ul>
-    <img width='100%' src={finalGif} alt='' />
-    <pre>{JSON.stringify(data, undefined, 2)}</pre>
-  </div>
-);
+export default App;
